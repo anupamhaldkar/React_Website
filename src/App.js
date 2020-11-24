36c686346f5a48c8c74fc09d6f6760877f250a4f
Anupam Haldkar <ahdev2020@outlook.com>
2020-11-24 14:31:06 +0530

    Wrapper and React Router installed ✡

--- src/App.js
+++ src/App.js
@@ -1,11 +1,18 @@
 import React, {Component} from 'react';
 import Wrapper from './components/Wrapper';
-
-
+import Home from './components/pages/Home';
+import {BrowserRouter as Router, Route, Link} from 'react-router-dom';
 class App extends Component {
   render(){
     return (
-      <Wrapper />
+      <Wrapper >
+        <Router>
+          <Route
+           path="/home"
+           component={Home}
+          />
+        </Router>
+        </Wrapper>
     )
   }
 }
--- src/components/Wrapper.js
+++ src/components/Wrapper.js
@@ -3,24 +3,26 @@ class Wrapper extends Component {
     render(){
         return (
             <div>
-            <nav class="navbar navbar-expand-lg navbar-dark fixed-top" id="mainNav">
-            <div class="container">
-                <a class="navbar-brand js-scroll-trigger" href="#page-top"><img src="assets/img/navbar-logo.svg" alt="" /></a>
-                <button class="navbar-toggler navbar-toggler-right" type="button" data-toggle="collapse" data-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation">
-                    Menu
-                    <i class="fas fa-bars ml-1"></i>
-                </button>
-                <div class="collapse navbar-collapse" id="navbarResponsive">
-                    <ul class="navbar-nav text-uppercase ml-auto">
-                        <li class="nav-item"><a class="nav-link js-scroll-trigger" href="#services">Services</a></li>
-                        <li class="nav-item"><a class="nav-link js-scroll-trigger" href="#portfolio">Portfolio</a></li>
-                        <li class="nav-item"><a class="nav-link js-scroll-trigger" href="#about">About</a></li>
-                        <li class="nav-item"><a class="nav-link js-scroll-trigger" href="#team">Team</a></li>
-                        <li class="nav-item"><a class="nav-link js-scroll-trigger" href="#contact">Contact</a></li>
-                    </ul>
-                </div>
-            </div>
-        </nav>
+           <nav class="navbar navbar-expand-lg navbar-dark navbar-custom fixed-top">
+    <div class="container">
+      <a class="navbar-brand" href="#">Start Bootstrap</a>
+      <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+      </button>
+      <div class="collapse navbar-collapse" id="navbarResponsive">
+        <ul class="navbar-nav ml-auto">
+          <li class="nav-item">
+            <a class="nav-link" href="#">Sign Up</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="#">Log In</a>
+          </li>
+        </ul>
+      </div>
+    </div>
+  </nav>
+
+        {this.props.children}
   </div>
         )
         }
